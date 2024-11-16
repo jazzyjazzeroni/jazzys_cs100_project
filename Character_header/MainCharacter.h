966d77f5e498d6e225bc610423b99a12880029a6
naira132 <nfaro002@ucr.edu>
2024-11-16 12:35:14 -0800

    edited mainc hcaracter.h

---
+++ Character_header/MainCharacter.h
@@ -0,0 +1,22 @@
+#ifndef MAINCHARACTER_H
+#define MAINCHARACTER_H
+
+#include "Character.h"
+using namespace std; 
+
+class MainCharacter : public Character {
+private:
+    string allegiance;
+    char move;
+
+public:
+    MainChar(const string &name, double health, double attackStrength, const string &allegiance);
+
+    void attack(Character &opponent) override;
+    void WitchInteraction();
+    char getMovement();
+   void setMovement(char);
+
+};
+
+#endif
