7fff6002995929fb056c21d506d83a0ba4df0523
naira132 <nfaro002@ucr.edu>
2024-11-18 03:02:24 -0800

    Added more unit test.cpp files

---
+++ Character_tests/Character_tests.cpp
@@ -0,0 +1,15 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "Character.h"
+
+using ::testing::AtLeast;
+using ::testing::Return;
+using ::testing::Mock;
+
+class MockCharacter : public Character{
+    MOCK_METHOD
+    MOCK_METHOD
+    MOCK_METHOD
+    MOCK_METHOD
+    
+}
