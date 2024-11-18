7f2a7d7f4456642c4e974edd62d9ffa3e3b17a73
naira132 <nfaro002@ucr.edu>
2024-11-18 13:16:41 -0800

    Added takes damage unit tests to mock class

--- Character_header/Character.h
+++ Character_header/Character.h
@@ -16,13 +16,14 @@ string name;
 CharType type;
 
 public:
+Character::Character();
 double getHealth () const;
-void setHealth(double);
-bool isalive() const;
+virtual void setHealth(double) =0;
+virtual bool isalive() const =0;
 Character(CharType, const string &, double, double);
-void damage(double dam);
-void takeDamage(double);
-virtual void displayStats();
+virtual void damage(double dam) = 0;
+virtual void takeDamage(double) = 0 ;
+virtual void displayStats() =0 ;
 virtual void attack(Character &) = 0;
 
 };
--- Character_tests/Character_tests.cpp
+++ Character_tests/Character_tests.cpp
@@ -7,9 +7,66 @@ using ::testing::Return;
 using ::testing::Mock;
 
 class MockCharacter : public Character{
-    MOCK_METHOD
-    MOCK_METHOD
-    MOCK_METHOD
-    MOCK_METHOD
-    
-}
+    public:
+    MockCharacter()
+        : Character(MAINCHAR, "MockCharacter", 100.0, 10.0) {}
+    MOCK_METHOD(void, displayStats, (), (override));
+    MOCK_METHOD(void, setHealth, (double), (override));
+    MOCK_METHOD(bool, isalive, (), (const, override));
+    MOCK_METHOD(void, damage, (double), (override));
+    MOCK_METHOD(void, takeDamage, (double), (override));
+    MOCK_METHOD(void, attack, (Character&), (override));
+
+};
+
+TEST(Charactertest, TakesDamagetimes){
+MockCharacter _char;
+EXPECT_CALL(_char, damage(40.0))
+    .Times(1);
+_char.damage(40.0);
+};
+
+TEST(Charactertest, TakesDamageNonZero){
+MockCharacter _char;
+_char.setHealth(15.0);
+EXPECT_CALL(_char, damage(40.0))
+    .Times(1);
+EXPECT_CALL(_char, setHealth(testing::Ge(0)))
+    .Times(1);
+_char.damage(40.0);
+EXPECT_DOUBLE_EQ(_char.getHealth(),0.0);
+};
+
+TEST(Charactertest, TakesDamagelowerHealth){
+MockCharacter _char;
+_char.setHealth(200.0);
+EXPECT_CALL(_char, setHealth(testing::_)).Times(1);
+EXPECT_CALL(_char, damage(50.0))
+    .Times(1);
+EXPECT_DOUBLE_EQ(_char.getHealth(), 150.0);
+};
+
+TEST(Charactertest, TakesZeroDamage){
+MockCharacter _char;
+_char.setHealth(200.0);
+_char.damage(0.0);
+EXPECT_DOUBLE_EQ(_char.getHealth(), 200.0);
+};
+
+
+
+
+
+TEST(Charactertest, TakesDamagetimes){
+MockCharacter _char;
+EXPECT_CALL(_char, damage(40.0))
+    .Times(1);
+_char.damage(40.0);
+};
+
+TEST(Charactertest, TakesDamagetimes){
+MockCharacter _char;
+EXPECT_CALL(_char, damage(40.0))
+    .Times(1);
+_char.damage(40.0);
+};
