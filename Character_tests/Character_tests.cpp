cea35cb8ee6aa1cc97b237506bb29da57b9487bd
naira132 <nfaro002@ucr.edu>
2024-11-18 16:45:39 -0800

    Added takes damage mock tests

--- Character_tests/Character_tests.cpp
+++ Character_tests/Character_tests.cpp
@@ -21,6 +21,7 @@ class MockCharacter : public Character{
 
 };
 
+//start health tests
 TEST(Charactertest, TestobtainingHealth){
 MockCharacter _char;
 EXPECT_EQ(_char.getHealth(), 90.0);
@@ -74,12 +75,77 @@ _char.damage(20.0);
     EXPECT_DOUBLE_EQ(_char.getHealth(), 180.0); 
 };
 
+TEST(Charactertest, HealthwithAliveTest){
+MockCharacter _char;
+ EXPECT_CALL(_char, setHealth(200.0))
+    .Times(1);
+EXPECT_CALL(_char, isalive())
+    .WillOnce(Return(true));
+_char.setHealth(200.0);
+EXPECT_TRUE(_char.isalive());
+EXPECT_CALL(_char, setHealth(0.0))
+    .Times(1);
+EXPECT_CALL(_char, isalive())
+    .WillOnce(Return(false));
+    _char.setHealth(0.0);
+    EXPECT_FALSE(_char.isalive()); 
+};
 
+//end health tests
 
+//start alive tests
+TEST(Charactertest, CharAlive){
+MockCharacter _char;
+EXPECT_CALL(_char, isalive())
+    .WillOnce(Return(true));
+EXPECT_TRUE(_char.isalive());
+};
 
+TEST(Charactertest, CharNotAlive){
+MockCharacter _char;
+EXPECT_CALL(_char, getHealth())
+.WillOnce(Return(0.0));
+EXPECT_CALL(_char, isalive())
+    .WillOnce(Return(false));
+EXPECT_FALSE(_char.isalive());
+};
 
+TEST(Charactertest, CharAlivelittleHealth){
+MockCharacter _char;
+EXPECT_CALL(_char, getHealth())
+.WillOnce(Return(0.1));
+EXPECT_CALL(_char, isalive())
+    .WillOnce(Return(true));
+EXPECT_TRUE(_char.isalive());
+};
 
+TEST(Charactertest, CharAliveafterDam){
+MockCharacter _char;
+EXPECT_CALL(_char, damage(20.0));
+EXPECT_CALL(_char, getHealth())
+    .WillOnce(Return(180.0));
+EXPECT_CALL(_char, isalive())
+    .WillOnce(Return(true));
+_char.damage(20.0);
+EXPECT_DOUBLE_EQ(_char.getHealth(), 180.0);
+EXPECT_TRUE(_char.isalive());
+};
 
+TEST(Charactertest, CharNotAliveafterDam){
+MockCharacter _char;
+EXPECT_CALL(_char, damage(300.0));
+EXPECT_CALL(_char, getHealth())
+    .WillOnce(Return(0.0));
+EXPECT_CALL(_char, isalive())
+    .WillOnce(Return(false));
+_char.damage(300.0);
+EXPECT_DOUBLE_EQ(_char.getHealth(), 0.0);
+EXPECT_FALSE(_char.isalive());
+};
+
+//end alive tests
+
+//start take damage tests
 TEST(Charactertest, TakesDamagetimes){
 MockCharacter _char;
 EXPECT_CALL(_char, damage(40.0))
@@ -113,3 +179,86 @@ _char.setHealth(200.0);
 _char.damage(0.0);
 EXPECT_DOUBLE_EQ(_char.getHealth(), 200.0);
 };
+//end take damage tests
+
+//start damage tests
+TEST(Charactertest, OpptakesZeroDamage){
+MockCharacter _char;
+MockCharacter opp;
+EXPECT_CALL(opp, getHealth())
+        .WillOnce(Return(150.0));
+    EXPECT_CALL(opp, setHealth(150.0))
+        .Times(1);
+    _char.damage(0.0);
+    opp.setHealth(150.0);
+    EXPECT_DOUBLE_EQ(opp.getHealth(), 150.0);
+};
+
+TEST(Charactertest, OpptakesFullDamage){
+MockCharacter _char;
+MockCharacter opp;
+EXPECT_CALL(opp, getHealth())
+        .WillOnce(Return(150.0));
+    EXPECT_CALL(opp, setHealth(0.0))
+        .Times(1);
+    _char.damage(150.0);
+    opp.setHealth(0.0);
+    EXPECT_DOUBLE_EQ(opp.getHealth(),0.0);
+};
+
+TEST(Charactertest, BelowZeroOpptest){
+MockCharacter _char;
+MockCharacter opp;
+EXPECT_CALL(opp, getHealth())
+        .WillOnce(Return(10.0));
+    EXPECT_CALL(opp, setHealth(0.0))
+        .Times(1);
+    _char.damage(30.0);
+    opp.setHealth(0.0);
+    EXPECT_DOUBLE_EQ(opp.getHealth(),0.0);
+};
+
+TEST(Charactertest, OpptakesDamage){
+MockCharacter _char;
+MockCharacter opp;
+EXPECT_CALL(opp, getHealth())
+        .WillOnce(Return(150.0));
+    EXPECT_CALL(opp, setHealth(100.0))
+        .Times(1);
+    _char.damage(50.0);
+    opp.setHealth(100.0);
+    EXPECT_DOUBLE_EQ(opp.getHealth(),100.0);
+};
+
+TEST(Charactertest, NegativeDamage){
+MockCharacter _char;
+MockCharacter opp;
+EXPECT_CALL(opp, getHealth())
+        .WillOnce(Return(150.0));
+    EXPECT_CALL(opp, setHealth(150.0))
+        .Times(1);
+    _char.damage(-50.0);
+    opp.setHealth(150.0);
+    EXPECT_DOUBLE_EQ(opp.getHealth(),150.0);
+};
+
+TEST(Charactertest, VeryLargeDamage){
+MockCharacter _char;
+MockCharacter opp;
+EXPECT_CALL(opp, getHealth())
+        .WillOnce(Return(150.0));
+    EXPECT_CALL(opp, setHealth(0.0))
+        .Times(1);
+    _char.damage(1000.0);
+    opp.setHealth(0.0);
+    EXPECT_DOUBLE_EQ(opp.getHealth(),0.0);
+};
+
+//end damage tests
+
+
+//start attack tests
+
+
+
+//end attack tests
