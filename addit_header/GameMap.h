0c187690a223ad0cd54feefea16ae64ab59555fe
naira132 <nfaro002@ucr.edu>
2024-11-22 01:22:27 -0800

    Added addition code to Map header removed object

--- addit_header/GameMap.h
+++ addit_header/GameMap.h
@@ -1,8 +1,7 @@
 #include <vector>
-#include "Object.h"
 #include "Swords.h"
-#include "Goblin.h"
-
+#include "Potions.h"
+#include "../Character_header/Goblin.h"
 #ifndef GAMEMAP_H
 #define GAMEMAP_H
 using namespace std;
@@ -13,15 +12,22 @@ private:
     int width;
     int numGoblin;
     int goblinKilled;
-    vector<vector<Object>> mapMatrix;
+    vector<vector<Sword>> SwdmapMatrix;
+    vector<vector<Potion>> PowmapMatrix;
+    vector<vector<Goblin>> GobmapMatrix;
+
 
 public:
     GameMap(const vector<vector<int>>& initMatrix, int w, int h);
     // void init(const vector<vector<int>>& layout);
  void init();
     void killGoblin(int x, int y);
-    Object& getObjectAt(int x, int y);
-    void setObjectAt(int x, int y, const Object& obj);
+    Sword& getSwordAt(int x, int y);
+    Potion& getPotionAt(int x, int y);
+    Goblin& getGoblinAt(int x, int y);
+    void setSwordAt(int x, int y, const Sword& Swd_obj);
+    void setPotionAt(int x, int y, const Potion& Pow_obj);
+    void setGoblint(int x, int y, const Goblin& Gob_obj);
     int getNumGoblins() const;
     int getGoblinsKilled() const;
 };
