42b450a878314e70e5ad37c94ecf5c1eb515beea
EMan115 <EmmanuelMemoGonzalez@gmail.com>
2024-11-21 16:00:57 -0800

    added a lot of the menus

--- addit_header/MenuPrinter.h
+++ addit_header/MenuPrinter.h
@@ -13,6 +13,19 @@ public:
 static void printMainMenu();
 static void printGoblinMenu(int left, int killed);
 static void printStatus(const MainCharacter& player);
+static void printSettingsMenu();
+static void printLanguageMenu();
+static void tutorialMenu();
+static void movementMenu();
+static void fireGoblinEncounterMenu();
+static void waterGoblinEncounterMenu();
+static void earthGoblinEncounterMenu();
+static void windGoblinEncounterMenu();
+static void playerTurnMenu(const MainCharacter& player);
+static void useItemsMenu();
+static void deathScreen();
+static void pauseMenu();
+static void quitReassuranceMenu();
 
 };
 #endif
