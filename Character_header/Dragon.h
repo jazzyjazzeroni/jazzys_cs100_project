cbc4f08e420a0c1e6e3fc5992b41565934558b3c
teez-ai <songcokurt.ad@gmail.com>
2024-11-27 13:46:40 -0800

    used this-> for dragon .cpp

--- Character_header/Dragon.h
+++ Character_header/Dragon.h
@@ -1,22 +1,31 @@
 #ifndef DRAGON_H
 #define DRAGON_H
 
-#include "Character.h"
+#include "../addit_header/GameMap.h"
+#include "../addit_header/Inventory.h"
+#include "../addit_header/MenuPrinter.h"
+#include "../Character_header/MainCharacter.h"
+#include "../Character_header/Character.h"
+#include "../Character_header/Dragon.h"
+#include <vector>
 
 class Dragon : public Character {
 private:
     int breathe_fire;
     int fire_ball;
+    MainCharacter& mc;
+    Dragon& dragon;
 
 public:
     Dragon();
     ~Dragon();
-    Dragon(const string &name, int pHealth, int pBreatheFire, int pFireBall);
-    int getfireAttack();
+    Dragon(MainCharacter& mc, Dragon& dragon,const string &name, int pHealth, int pBreatheFire, int pFireBall)    int getfireAttack();
     int getfireBall();
     void setfireBall(int pFireBall);
     void setfireAttack(int pBreatheFire);
     void attack(Character &opponent);
+    void startBattle();
+    void declareWinner();
     
 };
 
--- Character_src/Dragon.cpp
+++ Character_src/Dragon.cpp
@@ -1,4 +1,5 @@
 #include "Dragon.h"
+#include "MainCharacter.h"
 // private:
 //     int breatheFire;
 //     int fireBall;
@@ -54,11 +55,60 @@ void Dragon::setfireBall(int damage)
 }
 void Dragon::attack(Character &player)
 {
-    int attack_amount = this->breathe_fire;
-    if(fire_ball > 0)
+    if(this->fire_ball > 0)
     {
-        attack_amount = this->fire_ball;
+        this->attack_amount = this->fire_ball;
+    }
+
+    if(this->breathe_fire > 0)
+    {
+        this->attack_amount = this->breathe_fire;
     }
 }
+void Dragon::startBattle()
+    {
+        std::cout << "You have made it to the dragon's lair\n";
+        std::cout << "Will you be able to defeat Ignus?\n";
+        std::cout << "FINAL ROUND:\n";
 
+        while(mc.isalive() && dragon.isalive())
+        {
+            // MainCharacter move
+            std::cout << "Theodore attacks Ignus!\n";
+            mc.attack(dragon);
+            if(!dragon.isalive())
+            {
+                std::cout << "IGNUS HAS BEEN DEFEATED!\n";
+                break;
+            }
+
+            // MainCharacter move
+            std::cout << "Theodore attacks Ignus!\n";
+            dragon.attack(mc);
+            if(!mc.isalive())
+            {
+                std::cout << "You have died...\n";
+                break;
+            }
+
+            std::cout << "Theodore's health status: " << mc.getHealth() << '\n';
+            std::cout << "Ignus' health status: " << dragon.getHealth() << '\n';
+        }
+
+        declareWinner();
+    }
+
+void Dragon::declareWinner()
+    {
+        if(mc.isalive() && !dragon.isalive())
+        {
+            std::cout << "You have slained Ignus! YOU WON THE GAME\n";
+        }
+        else if(!mc.isalive() && dragon.isalive())
+        {
+            std::cout << "Ignus has won. Try again? y/n \n"; 
+        }
+        else
+            std::cout << "Both characters have been defeated. DRAW!\n";
+    }
 
