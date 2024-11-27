34b7831df0319af4c57523423c6fd6322f3e5cd6
teez-ai <songcokurt.ad@gmail.com>
2024-11-26 22:15:24 -0800

    Progress for game manager

--- addit_header/GameManager.h
+++ addit_header/GameManager.h
@@ -15,7 +15,23 @@ private:
 public:
     GameManager();
     ~GameManager();
+
+    //Initializes the starting room, player, and levels
     void gameStart();
+
+    // Gets user input (hit w, hit a, hit s, hit d, "take sword", "take potion", "fight goblin")
+    // This function parses the input and determines the approporiate action
+    void handleInput();
+
+    // nextLevel()
+    // update()
+    //updates inventory
+    // will trigger the event with goblin
+    // 
+    void updateLevel();
+
+    // will start the level against the final boss
+    void finalBoss();
     void pause();
     void quit();
 };
