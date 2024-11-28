1e0f982dce6b390c70b73ec533b8589e3aef9555
naira132 <nfaro002@ucr.edu>
2024-11-28 11:22:44 -0800

    Added functionla swords and potions

--- addit_src/Potions.cpp
+++ addit_src/Potions.cpp
@@ -25,6 +25,10 @@ void Potion::setType(const string &potionType) {
     type = potionType;
 }
 
+void Potion::print() const {
+    cout << "Name: " << type << ", Value: " << healingAmount << endl;
+}
+
 void Potion::healCharacter(MainCharacter &character) {
     if (type == "Small Potion") {
         character.heal(50); 
@@ -34,3 +38,23 @@ void Potion::healCharacter(MainCharacter &character) {
         character.heal(healingAmount); 
     }
 }
+
+// int main() {
+//     // Create a default potion
+//     Potion defaultPotion;
+//     cout << "Default Potion:" << endl;
+//     defaultPotion.print();
+
+//     // Create a healing potion with specific properties
+//     Potion healingPotion(50, "Healing Potion");
+//     cout << "\nCustom Potion:" << endl;
+//     healingPotion.print();
+
+//     // Modify the default potion
+//     defaultPotion.setHealingAmount(30);
+//     defaultPotion.setType("Small Healing Potion");
+//     cout << "\nModified Default Potion:" << endl;
+//     defaultPotion.print();
+
+//     return 0;
+// }
