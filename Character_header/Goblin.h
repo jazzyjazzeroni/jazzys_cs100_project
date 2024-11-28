6d892c459badac99b8be939fa782fc00d149bc27
teez-ai <songcokurt.ad@gmail.com>
2024-11-28 14:09:34 -0800

    Modify unit test in Goblin

---
+++ Character_header/Goblin.h
@@ -0,0 +1,25 @@
+#ifndef GOBLIN_H
+#define GOBLIN_H
+
+#include "Character.h"
+using namespace std; 
+
+class Goblin : public Character {
+private:
+    int specialPower;
+
+public:
+    Goblin(const string &name, int health, int attackStrength, const string &allegiance);
+    Goblin(int);
+    Goblin();
+    int getHealth() const ;
+    CharType getType() ;
+    void setHealth(int) ;
+    bool isalive() const ;
+    void dealtDamage(int) ;
+    void recieveDamage(int) ;
+    void attack(Character &player) override;
+    
+};
+
+#endif
