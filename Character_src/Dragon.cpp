916822600aa6f820dd1f4245b36d6fdc3e6f9b2c
teez-ai <songcokurt.ad@gmail.com>
2024-11-27 20:14:39 -0800

    Modify dragon

--- Character_src/Dragon.cpp
+++ Character_src/Dragon.cpp
@@ -1,4 +1,5 @@
 #include "Dragon.h"
+#include "MainCharacter.h"
 // private:
 //     int breatheFire;
 //     int fireBall;
@@ -12,21 +13,23 @@
 //     void attack(Character &opponent);
 
 
-Dragon::Dragon(const string &name, int pHealth, int pBreatheFire, int pFireBall)
-{
-   this->name = "Ignus";
-   this->health = health;
-   this->breathe_fire = pBreatheFire;
-   this->fire_ball = pFireBall;
+Dragon::Dragon(MainCharacter& mc, const string &name, int pHealth, int pBreatheFire, int pFireBall)
+    : mc(mc), breathe_fire(pBreatheFire), fire_ball(pFireBall) {
+    this->name = name;
+    health = pHealth;
 }
 
-Dragon::Dragon()
-{
-    this->name = " ";
-    this->health = 0;
-    this->fire_ball = 0;
-    this->breathe_fire = 0;
+void Dragon::setfireAttack(int breatheFire){
+    this->breathe_fire = breatheFire;
 }
+
+// Dragon::Dragon()
+// {
+//     this->name = " ";
+//     this->health = 0;
+//     this->fire_ball = 0;
+//     this->breathe_fire = 0;
+// }
 
 Dragon::~Dragon()
 {
@@ -54,11 +57,62 @@ void Dragon::setfireBall(int damage)
 }
 void Dragon::attack(Character &player)
 {
-    int attack_amount = this->breathe_fire;
-    if(fire_ball > 0)
+    if(this->fire_ball > 0)
     {
-        attack_amount = this->fire_ball;
+        // this->attack_amount = this->fire_ball;
+        player.takeDamage(this->fire_ball);
+    }
+
+    if(this->breathe_fire > 0)
+    {
+        // this->attack_amount = this->breathe_fire;
+        player.takeDamage(this->breathe_fire);
     }
 }
+void Dragon::startBattle()
+    {
+        std::cout << "You have made it to the dragon's lair\n";
+        std::cout << "Will you be able to defeat Ignus?\n";
+        std::cout << "FINAL ROUND:\n";
 
+        while(mc.isalive() && this->isalive())
+        {
+            // MainCharacter move
+            std::cout << "Theodore attacks Ignus!\n";
+            mc.attack(*this); //maybe not should be okay bc this pointer
+            if(!this->isalive())
+            {
+                std::cout << "IGNUS HAS BEEN DEFEATED!\n";
+                break;
+            }
+
+            // MainCharacter move
+            std::cout << "Theodore attacks Ignus!\n";
+            this->attack(mc);
+            if(!mc.isalive())
+            {
+                std::cout << "You have died...\n";
+                break;
+            }
+
+            std::cout << "Theodore's health status: " << mc.getHealth() << '\n';
+            std::cout << "Ignus' health status: " << this->getHealth() << '\n';
+        }
+
+        declareWinner();
+    }
+
+void Dragon::declareWinner()
+    {
+        if(mc.isalive() && !this->isalive())
+        {
+            std::cout << "You have slained Ignus! YOU WON THE GAME\n";
+        }
+        else if(!mc.isalive() && this->isalive())
+        {
+            std::cout << "Ignus has won. Try again? y/n \n"; 
+        }
+        else
+            std::cout << "Both characters have been defeated. DRAW!\n";
+    }
 
