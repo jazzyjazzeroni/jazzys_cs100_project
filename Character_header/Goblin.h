3307bd5d43f75ccf2dd41b105b8f88b95b48e13a
teez-ai <songcokurt.ad@gmail.com>
2024-12-03 16:31:54 -0800

    Fix more merge conflicts

--- Character_header/Goblin.h
+++ Character_header/Goblin.h
@@ -1,58 +1,27 @@
-#include "../Character_header/Goblin.h"
+#ifndef GOBLIN_H
+#define GOBLIN_H
 
-using namespace std;
-// the goblin constructor i believe
-Goblin::Goblin(const std::string &name, int health, int damage, const std::string &element)
-    : Character(GOBLIN, name, health, damage, element), powers(element), currentElement(element) {
-    if (element != "Fire" && element != "Water" && element != "Earth" && element != "Air") {
-        throw std::invalid_argument("Unknown power type: " + element);
-    }
-}
+#include "Character.h"
 
+using namespace std; 
 
-// returns goblin's current health
-int Goblin::getHealth() const {
-    return health;
-}
+class Goblin : public Character{
+    private:
+        Powers powers;
+        string currentElement;
+        
 
- std::string Goblin::getGoblinType() const {
-        return currentElement;  // Return the type of the goblin (e.g., "Fire")
-    }
+public:
+    Goblin(const std::string &name, int health, int damage, const std::string &element);
+    Goblin(){};
+    int getHealth() const;
+    string getType() const override ;
+    string getGoblinType() const;
+    void setHealth(int);
+    bool isalive() const;
+    void recieveDamage(int);
+    void attack(Character &player) override;
+    void print() const override;
+};
 
-// returns goblin's type of element maybe?
-string Goblin::getType() const{
-    return "Goblin";
-}
-
-// set's goblin's health
-void Goblin::setHealth(int health) {
-    this->health = health;
-}
-
-// returns true if goblin is still alive
-bool Goblin::isalive() const {
-    return (getHealth() > 0);
-}
-
-
-void Goblin::recieveDamage(int dam) {
-    this->health -= dam;
-    if (health < 0) {
-        health = 0;
-    }
-}
-
-
-
-// // probably used when the goblin goes to attack the player
-void Goblin::attack(Character &target) {
-    cout << getType() << " attacks " << target.getType() << "!" << endl;
-
-    int targetHealth = target.getHealth();
-    target.setHealth(targetHealth - powers.calculateDamage());
-    cout << target.getType() << "'s health after attack: " << target.getHealth() << endl;
-}
-
-void Goblin::print() const {
-    cout << "Goblin: " << name << ", Health: " << health << ", Attack: " << attackAmount << endl;
-}
+#endif
