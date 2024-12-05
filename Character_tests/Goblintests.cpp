0c25775b2358c35775f05882f6f2aef762c4271c
EMan115 <EmmanuelMemoGonzalez@gmail.com>
2024-12-05 13:05:38 -0800

    created constructor test for water goblins

--- Character_tests/Goblintests.cpp
+++ Character_tests/Goblintests.cpp
@@ -2,7 +2,7 @@
 #include "../Character_header/Goblin.h"
 
 // Test: Constructor Initialization for fire goblins
-TEST(GoblinTestSuite, testDefaultConstructor) {
+TEST(GoblinTestSuite, testFireGobDefaultConstructor) {
     Goblin gob("Goblin", 100, 15, "Fire");
 
     EXPECT_EQ(gob.getHealth(), 100);
@@ -10,3 +10,14 @@ TEST(GoblinTestSuite, testDefaultConstructor) {
     EXPECT_EQ(gob.getGoblinType(), "Fire");
 
 }
+
+// Test: Constructor Initialization for water goblins
+TEST(GoblinTestSuite, testWaterGobDefaultConstructor) {
+    Goblin gob("Goblin", 75, 10, "Water");
+
+    EXPECT_EQ(gob.getHealth(), 75);
+    EXPECT_EQ(gob.getType(), "Goblin");
+    EXPECT_EQ(gob.getGoblinType(), "Water");
+
+}
+
