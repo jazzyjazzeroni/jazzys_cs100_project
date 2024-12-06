4db4176b30840705fe62200e3181b255feb53522
EMan115 <EmmanuelMemoGonzalez@gmail.com>
2024-12-06 11:33:47 -0800

    did a thing that fixed a thing

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/csmajs/egonz291/final-project-the-processing-units
+# Build directory: /data/home/csmajs/egonz291/final-project-the-processing-units
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googletest")
