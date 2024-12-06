ffdc93208af468ecc4f767fcc7a078756bae09cd
teez-ai <songcokurt.ad@gmail.com>
2024-12-05 22:42:23 -0800

    Add animation files

---
+++ addit_header/Animations.h
@@ -0,0 +1,13 @@
+#ifndef ANIMATIONS_H
+#define ANIMATIONS_H
+
+#include <string>
+
+class Animation {
+public:
+    static void printGoblinArt();
+    static void printSmallPotionArt();
+    static void printBigSwordArt();
+    static void printSmallSwordArt();
+};
+#endif
